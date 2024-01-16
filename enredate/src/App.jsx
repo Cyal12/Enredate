536acc66f1dc988a4709d7de70f84c477acedbdf
IRISALINA <irisalinap@gmail.com>
2024-01-16 15:27:46 -0600

    creacion de rutas

--- enredate/src/App.jsx
+++ enredate/src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Prueba from './Prueba'
+import Navbar from './Navbar'
+import Home from './Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>hola</h1>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/prueba' element={<Prueba/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
