242351836907016a63831ee3a632d62eb6726766
IRISALINA <irisalinap@gmail.com>
2024-01-18 14:03:03 -0600

    CHANGES

--- enredate/src/App.jsx
+++ enredate/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Prueba from './Prueba'
 import Navbar from './Navbar'
 import Home from './Home'
+import CreateAccount from './CreateAccount'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -15,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/prueba' element={<Prueba/>}/>
+          <Route path='/createAccount' element={<CreateAccount/>}/>
         </Routes>
       </BrowserRouter>
     </>
---
+++ enredate/src/CreateAccount.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const CreateAccount = () => {
+  return (
+    <div className='createAccount'>
+      
+      <header>
+        <h1>Crea tu cuenta</h1>
+        <small>¿Ya tienes una cuenta?</small>
+      </header>
+
+    <div>
+        <div className='option'>
+            <img src="https://cdn-icons-png.flaticon.com/128/10124/10124147.png" alt="" />
+            <p>Continua con Google</p>
+        </div>
+    </div>
+
+    </div>
+  )
+}
+
+export default CreateAccount
