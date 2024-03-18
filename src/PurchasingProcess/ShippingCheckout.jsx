640414e1b097372a6b649703d03a26009184e5b2
IRISALINA <irisalinap@gmail.com>
2024-03-18 13:24:16 -0600

    All folders and files added
    All folders and files added

---
+++ src/PurchasingProcess/ShippingCheckout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ShippingCheckout = () => {
+  return (
+    <div>
+      Shipping checkout
+      Checkuout de envio
+    </div>
+  )
+}
+
+export default ShippingCheckout
