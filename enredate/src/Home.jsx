56130b71af2812a4486f3c976503a81908e1d50f
IRISALINA <irisalinap@gmail.com>
2024-03-17 18:01:27 -0600

    iris inicial structure
    Basic routing and fixes

--- enredate/src/Home.jsx
+++ enredate/src/Home.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { SegmentedControl } from '@mantine/core';
+import { Table } from '@mantine/core';
 
 const Home = () => {
+
+  const elements = [
+    { position: 6, mass: 12.011, symbol: 'C', name: 'Carbon' },
+    { position: 7, mass: 14.007, symbol: 'N', name: 'Nitrogen' },
+    { position: 39, mass: 88.906, symbol: 'Y', name: 'Yttrium' },
+    { position: 56, mass: 137.33, symbol: 'Ba', name: 'Barium' },
+    { position: 58, mass: 140.12, symbol: 'Ce', name: 'Cerium' },
+  ];
+
+  const rows = elements.map((element) => (
+    <Table.Tr key={element.name}>
+      <Table.Td>{element.position}</Table.Td>
+      <Table.Td>{element.name}</Table.Td>
+      <Table.Td>{element.symbol}</Table.Td>
+      <Table.Td>{element.mass}</Table.Td>
+    </Table.Tr>
+  ));
   return (
     <div>
       <body>
@@ -25,6 +44,25 @@ const Home = () => {
         </Link>
         </div>
       </body>
+
+    <div>
+
+      <Table>
+      <Table.Thead>
+        <Table.Tr>
+          <Table.Th>Nombre</Table.Th>
+          <Table.Th>Cantidad</Table.Th>
+          <Table.Th>Estatus</Table.Th>
+          <Table.Th>Comentarios</Table.Th>
+        </Table.Tr>
+      </Table.Thead>
+      <Table.Tbody>{rows}</Table.Tbody>
+    </Table>
+
+    <SegmentedControl data={['Día', 'Semana', 'Mes', 'Año']} />
+
+    </div>
+    
     </div>
   )
 }
